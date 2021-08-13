a12b898bf61c512db06cc0463458a9c95f9c9132
SeoJW <wodnjs1000gk@naver.com>
2021-08-13 14:10:36 +0900

    board 2-12 - 파일첨부 수정/삭제 (Code)

--- board/routes/posts.js
+++ board/routes/posts.js
@@ -375,7 +375,9 @@ router.get('/:id/edit', util.isLoggedin, checkPermission, function(req, res){
   var post = req.flash('post')[0];
   var errors = req.flash('errors')[0] || {};
   if(!post){
-    Post.findOne({_id:req.params.id}, function(err, post){
+    Post.findOne({_id:req.params.id})                           // 1
+      .populate({path:'attachment',match:{isDeleted:false}})    // 1
+      .exec(function(err, post){
         if(err) return res.json(err);
         res.render('posts/edit', { post:post, errors:errors });
       });
@@ -387,22 +389,20 @@ router.get('/:id/edit', util.isLoggedin, checkPermission, function(req, res){
 });
 
 // update
-router.put('/:id', util.isLoggedin, checkPermission, function(req, res){
+router.put('/:id', util.isLoggedin, checkPermission, upload.single('newAttachment'), async function(req, res){
+  var post = await Post.findOne({_id:req.params.id}).populate({path:'attachment',match:{isDeleted:false}});
+  if(post.attachment && (req.file || !req.body.attachment)){
+    post.attachment.processDelete();
+  }
+  req.body.attachment = req.file?await File.createNewInstance(req.file, req.user._id, req.params.id):post.attachment;
   req.body.updatedAt = Date.now();
   Post.findOneAndUpdate({_id:req.params.id}, req.body, {runValidators:true}, function(err, post){
     if(err){
       req.flash('post', req.body);
       req.flash('errors', util.parseError(err));
-      /*
-board08 - Post Error에서 Post.findOneAndUpdate에
-{runValidators:true}이 추가되었습니다..
-findOneAndUpdate는 기본설정이 schema에 있는 validation을
-작동하지 않도록 되어 있기때문에 이 option을 통해서
-validation이 작동하도록 설정해 주어야 합니다.
-    */
-    return res.redirect('/posts/'+req.params.id+'/edit'+res.locals.getPostQueryString()); // 1
-  }
-  res.redirect('/posts/'+req.params.id+res.locals.getPostQueryString());
+      return res.redirect('/posts/'+req.params.id+'/edit'+res.locals.getPostQueryString());
+    }
+    res.redirect('/posts/'+req.params.id+res.locals.getPostQueryString());
   });
 });
 
